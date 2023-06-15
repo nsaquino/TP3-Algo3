cdfca9516a035174cc2363f99702b3457027dcc2
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-06-15 00:57:28 -0300

    Ej1 ver como hacer el min_heap

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <tuple>
 #include <math.h>
+#include <queue>
 
 //using ll = long long;
 using namespace std;
@@ -16,17 +17,28 @@ vector<tuple<node_t, node_t, weight_t>> candidatos;
 vector<bool> S; //S = conjunto de vertices ya "evaluados"
 vector<int> dist; //vector de distancias mínimas de s a todos
 
+bool comp(pair<int, int> a, pair<int, int> b){
+    return a.second > b.second;
+}
+
 void dijkstra(node_t s){
+    priority_queue<pair<int, int>, vector<pair<int, int>>, comp<pair<int, int>>> min_heap;
+
     for (int i = 0; i < N; i++)
         dist[i] = INFINITY;
     S[s] = true;
     dist[s] = 0;
     for (auto [v, l] : adj[s]) {
-        dist[v] = l;
+        dist[v] = l; //l = c(s,v)
     }
 
-    
-
+    for (int count = 0; count < N; count++){
+        v = min_heap();
+        for (auto [x, l] : adj[v]) {
+            if(dist[s,x] > dist[s,v] + w(v->x))
+                dist[s,x] = dist[s,v] + w(v->x)
+        }
+    }
 }
 
 int main() {
